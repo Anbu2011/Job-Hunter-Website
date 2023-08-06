0916e0f572362d6d3f3e4dc4e0f6673dbad7a1f9
Anbu2011 <111184112+Anbu2011@users.noreply.github.com>
2023-08-06 05:43:42 +0000

    Added HTML file

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask #flask - module and Flask - class inside the module & importing functionalities from flask
-app = Flask(__name__) #creating obj and assigning Flask functionalities into app and __name__ - script invoked __main__. 
-@app.route("/") #part of the url coming after the domain name
+from flask import Flask , render_template
+
+app = Flask(__name__) 
+
+@app.route("/") 
 
 def hello():
-  return "hellognugjrgr"
+  return render_template("home.html")
+  
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
   
